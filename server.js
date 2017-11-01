0b305c18905fe8f53807a83461e519393559a4e3
Chance O'Neal <chancelor.oneal@gmail.com>
2017-11-01 11:41:11 -0700

    Big changes!
    — Added the ability to stack draw twos/draw fours and either force
    players to draw the running total or to play a draw two/draw four.
    — Added a prompt for users who either need to draw x amount of cards or
    play a draw two/draw four.
    — Added additional functionality where if a player is required to draw
    x amount of cards, clicking on the “Draw Card” button will
    automatically draw x amount of cards for them (same with the “Draw 2”
    and “Draw 4” buttons. If a player clicks either of those buttons and
    they don’t *need* to draw that many, they will be given that number
    regardless).
    — Disallowed players from drawing when not their turn.
    — Disallowed players from starting a new game in the middle of an
    ongoing game.
    — Added function comments.

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ var unoDeck = [];
 var playedCards = [];
 var players = [];
 var numPlayers = 0;
-var turns;
+var turns = 0;
+var lastCardDrawX = false;
+var drawTotal = 0;
 
 // Helpful Comment:
 // io talks to everybody connected to the server
@@ -39,36 +41,76 @@ io.on('connection', function(socket){
         numPlayers--;
     });
     
-    socket.on('new game', function(){
-        io.emit('message', socket.username + " started a new game.");
-        // Get a shuffled uno deck
-        unoDeck = deckHandler();
-        var tempCard = unoDeck.pop();
-        playedCards.push(tempCard);
-        turns = 0;
-        if (tempCard.value == "wild" || tempCard.value == "draw four") {
-            io.emit('message', "The curent card is a " + tempCard.value + " card. The color is " + tempCard.color + ".");
-            io.emit('display card', tempCard);
+    socket.on('new game', function() {
+        if (turns == 0) {
+            io.emit('message', socket.username + " started a new game.");
+            // Get a shuffled uno deck
+            unoDeck = deckHandler();
+            var tempCard = unoDeck.pop();
+            playedCards.push(tempCard);
+            turns = 0;
+            if (tempCard.value == "wild" || tempCard.value == "draw four") {
+                io.emit('message', "The curent card is a " + tempCard.value + ". The color is " + tempCard.color + ".");
+                io.emit('display card', tempCard);
+            } else {
+                io.emit('message', "The current card is a " + tempCard.color + " " + tempCard.value + ".");
+                io.emit('display card', tempCard);
+            }
+
+            lastCardDrawX = (tempCard.value == "draw four" || tempCard.value == "draw two");
+            if (lastCardDrawX) {
+                drawTotal = (tempCard.value == "draw two") ? 2 : 4;
+            }
+
+            whosTurn();
         } else {
-            io.emit('message', "The current card is a " + tempCard.color + " " + tempCard.value + ".");
-            io.emit('display card', tempCard);
+            io.emit('message', socket.username + " attempted to start a new game in the middle of this game!");
         }
-        whosTurn();
     });
     
     socket.on('card get', function(msg){
-        var tempCard = cardDrawHandler();
-        io.emit('message', socket.username + " drew a card.");
-        socket.emit('card get', tempCard);
+        if (!isPlayerTurn(socket.id) && turns > 0) {
+            io.emit('message', socket.username + " attempted to play out of turn.");
+        } else if (lastCardDrawX && drawTotal > 1) {
+            for (var i = 0; i < drawTotal; i++) {
+                var tempCard = cardDrawHandler();
+                socket.emit('card get', tempCard);
+            }
+
+            io.emit('message', socket.username + " drew " + drawTotal + " cards.");
+            lastCardDrawX = false;
+            drawTotal = 0;
+            turns++;
+            whosTurn();
+        } else {
+            var tempCard = cardDrawHandler();
+            io.emit('message', socket.username + " drew a card.");
+            socket.emit('card get', tempCard);
+        }
     });
     
     socket.on('card get x', function(x) {
-        for (var i = 0; i < x; i++) {
-            var tempCard = cardDrawHandler();
-            socket.emit('card get', tempCard);
+        if (!isPlayerTurn(socket.id) && turns > 0) {
+            io.emit('message', socket.username + " attempted to play out of turn.");
+        } else if (lastCardDrawX && drawTotal >= x) {
+            for (var i = 0; i < drawTotal; i++) {
+                var tempCard = cardDrawHandler();
+                socket.emit('card get', tempCard);
+            }
+            
+            io.emit('message', socket.username + " drew " + drawTotal + " cards.");
+            lastCardDrawX = false;
+            drawTotal = 0;
+            turns++;
+            whosTurn();
+        } else {
+            for (var i = 0; i < x; i++) {
+                var tempCard = cardDrawHandler();
+                socket.emit('card get', tempCard);
+            }
+            
+            io.emit('message', socket.username + " drew " + x + " cards.");
         }
-        
-        io.emit('message', socket.username + " drew " + x + " cards.");
     });
 
     socket.on('card play', function(card){
@@ -79,13 +121,39 @@ io.on('connection', function(socket){
         var lastPlayed = playedCards.slice(-1)[0];
 
         if (isPlayerTurn(socket.id)) {
+            if (lastCardDrawX && card.value != "draw two" && card.value != "draw four") {
+                // socket.emit('card get', card);
+                // for (var i = 0; i < drawTotal; i++) {
+                //     var tempCard = cardDrawHandler();
+                //     socket.emit('card get', tempCard);
+                // }
+                // io.emit('message', socket.username + " drew " + drawTotal + " cards.");
+                // lastCardDrawX = false;
+                // drawTotal = 0;
+                // turns++;
+                // whosTurn();
+                // return;
+
+                io.emit('message', socket.username + " tried to play a " + card.color + " " + card.value + ". They must either play a draw two, a draw four, or draw " + drawTotal + " cards.");
+                socket.emit('card get', card);
+                return;
+            }
 
             // Handle matching values, colors, or wild cards
             if (lastPlayed.value == card.value || lastPlayed.color == card.color || 
                 card.value == "wild" || card.value == "draw four") {
                 playedCards.push(card);
+
+                lastCardDrawX = (card.value == "draw two" || card.value == "draw four");
+                if (lastCardDrawX) {
+                    drawTotal += (card.value == "draw two") ? 2: 4;
+                } else {
+                    drawTotal = 0;
+                }
+
             } else if (lastPlayed.value == "draw four" && card.value == "draw two") {
                 playedCards.push(card);
+                drawTotal += 2;
             } else {
                 // Card was not valid
                 io.emit('message', socket.username + " tried to play a " + card.color + " " + card.value + ", an action deemed invalid. Card was returned to hand.");
@@ -94,7 +162,7 @@ io.on('connection', function(socket){
             }
             
             // Handle message if wild card or not
-            if (card.value == "wild" || card.value == "draw four") {     
+            if (card.value == "wild" || card.value == "draw four") {
                 io.emit('message', socket.username + " played a " + card.value + ". The color is now " + card.color + ".");
                 io.emit('display card', card);
             } else {
@@ -107,6 +175,7 @@ io.on('connection', function(socket){
                     turns++;
                 }
             }
+
             turns++;
             whosTurn();
         } else {
@@ -121,13 +190,19 @@ io.on('connection', function(socket){
             io.emit('message', socket.username + " selected undo. This is not an option at the moment, so nothing will be done.");
         } else {
             var undoCard = playedCards.pop();
+            if (undoCard.value == "draw two" || undoCard.value == "draw four") {
+                drawTotal -= (undoCard.value == "draw two") ? 2 : 4;
+                var lastValue = playedCards[playedCards.length - 1].value;
+                lastCardDrawX = (lastValue == "draw two" || lastValue == "draw four");
+            }
             socket.emit('card get', undoCard);
             io.emit('message', socket.username + " selected undo and the " + undoCard.color + " " + undoCard.value + " was returned to hand.");
             io.emit('display card', playedCards.slice(-1)[0]);
+            turns--;
+            whosTurn();
         }
-        turns--;
     });
-    
+
     socket.on('call uno', function() {
         io.emit('message', socket.username + " has called Uno!");
     });
@@ -139,10 +214,15 @@ io.on('connection', function(socket){
     socket.on('victory', function() { 
         io.emit('message', socket.username + " has won. Congratulations!");
         io.emit('card clear');
+        turns = 0;
+        drawTotal = 0;
+        lastCardDrawX = false;
     });
 });
 
-// This function handles drawing a card and returning it to the player
+/**
+ * Handles drawing a card and returning it to the player.
+ */
 function cardDrawHandler() {
     if (unoDeck.length < 1) {
         unoDeck = deckHandler();
@@ -151,7 +231,9 @@ function cardDrawHandler() {
     return tempCard;
 }
 
-// Creates and returns a full Uno deck
+/**
+ * Creates and returns a full Uno deck.
+ */
 function deckHandler() {
     var tempDeck = [];
     var colors = ["red", "blue", "green", "yellow"];
@@ -165,17 +247,24 @@ function deckHandler() {
     return tempDeck;
 }
 
+/**
+ * Checks if it is the turn of the player who attempted to play a card.
+ * @param {String} id 
+ */
 function isPlayerTurn(id) {
     return id === players[turns % numPlayers].id;
 }
 
+/**
+ * Relays a global message indicating which player's turn it is.
+ */
 function whosTurn() {
     var user = players[turns % numPlayers].username;
     io.emit('message', user + "'s turn.");
 }
 
 /**
- * Shuffles array in place. ES6 version
+ * Shuffles array in place. ES6 version.
  * @param {Array} a items The array containing the items.
  */
 function shuffle(a) {
